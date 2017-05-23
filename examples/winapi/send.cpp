672f7719db0be0f907465e347ff4cc8c1430fe7e
DreamSworK <sergiodreamswork@gmail.com>
2017-05-23 05:18:10 -0400

    Update send.cpp

--- examples/winapi/send.cpp
+++ examples/winapi/send.cpp
@@ -6,19 +6,19 @@
 #define PORT 49152
 
 int main() {
-	SOCKET sock;
-	WSADATA wsa;
+    SOCKET sock;
+    WSADATA wsa;
     struct sockaddr_in addr;
     if (WSAStartup(MAKEWORD(2,2), &wsa) == 0) {
-		if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) != SOCKET_ERROR) {
-			memset((char*)&addr, 0, sizeof(addr));
-			addr.sin_family = AF_INET;
-			addr.sin_port = htons(PORT);
-			addr.sin_addr.S_un.S_addr = inet_addr(SERVER);
-			const char message[] = "Message";
-			sendto(sock, message, strlen(message) , 0 , (struct sockaddr*)&addr, sizeof(addr));
-			closesocket(sock);
-		}
+	if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) != SOCKET_ERROR) {
+		memset((char*)&addr, 0, sizeof(addr));
+		addr.sin_family = AF_INET;
+		addr.sin_port = htons(PORT);
+		addr.sin_addr.S_un.S_addr = inet_addr(SERVER);
+		const char message[] = "Message";
+		sendto(sock, message, strlen(message), 0, (struct sockaddr*)&addr, sizeof(addr));
+		closesocket(sock);
+	}
     }
     WSACleanup();
     return 0;
