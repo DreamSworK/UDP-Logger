40efc4f564c91c98c5227754b7e22057ac17405f
DreamSworK <sergiodreamswork@gmail.com>
2017-05-25 16:07:15 -0400

    Add C# UDP send example

---
+++ examples/C#/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System.Net.Sockets;
+
+namespace UdpSend
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            using (UdpClient udpClient = new UdpClient("127.0.0.1", 49152))
+            {
+                for (int i = 0; i < 10; ++i) {
+                    string message = "Message " + i;
+                    byte[] dgram = Encoding.ASCII.GetBytes(message);
+                    udpClient.Send(dgram, dgram.Length);
+                }
+            }
+        }
+    }
+}
